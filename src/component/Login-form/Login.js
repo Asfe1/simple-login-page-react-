35e5d5cea40ba36514ef4b76e342ede5ea93d209
Asfe1 <aarafath172154@bscse.uiu.ac.bd>
2022-03-22 14:08:00 +0600

    updated login page added

---
+++ src/component/Login-form/Login.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Login.css'
+import logo from '../images/blue-logo 1.png'
+import img1 from '../images/FB.png'
+import img2 from '../images/google.png'
+
+//  
+const Login = () => {
+    return (
+        <div className='background d-flex flex-row-reverse '>
+            <div className='col-lg-1'></div>
+            <form action="" id='form' className='border text-center bg-white col-lg-4 '>
+                <div >
+                    <img className='img-fluid' src={logo} id="logo" alt="" />
+                    <p id='p1'>Explore The Best Tours... Hurry up!!</p>
+                    <input className='input' type="email" name="" id="" placeholder='Email Address' />
+                    <br />
+                    <input className='input' type="password" name="" id="" placeholder='Password' />
+                    <a href="/#"><small>Forgot Password?</small></a>
+                    <br />
+
+                    <button type="button" class="btn ">Log In</button>
+                    <div className='d-flex justify-content-center py-4'>
+                        <hr className='' />
+                        <p className='px-3 fw-bold pt-1'> or </p>
+                        <hr className='' />
+                    </div >
+                    <div className='d-flex justify-content-center'>
+                        <a href="/#"> <img className='img' src={img1} alt="" /></a>
+                        <a href="/#"> <img className='img' src={img2} alt="" /></a>
+                    </div>
+                    <p id='bottom-p1'>Don't have an account? <a href="/#">Sign Up</a> </p>
+                </div>
+
+            </form>
+        </div>
+    );
+};
+
+export default Login;
